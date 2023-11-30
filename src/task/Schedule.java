eab2e1287052968d0938b9023422c2d6fa446e72
Fahrian Afdholi <13521031@std.stei.itb.ac.id>
2023-11-30 17:18:49 +0700

    update: Schedule.java, Task.java

--- src/task/Schedule.java
+++ src/task/Schedule.java
@@ -1,4 +1,28 @@
 package task;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Schedule {
+    private Map<Integer, List<Task>> schedule = new HashMap<>();
+
+    public Schedule(Task[] tasks){
+        for (Task task : tasks){
+            if (this.schedule.containsKey(task.getSchedule())){
+                this.schedule.get(task.getSchedule()).add(task);
+            }else {
+                this.schedule.put(task.getSchedule(), new ArrayList<>());
+                this.schedule.get(task.getSchedule()).add(task);
+            }
+        }
+    }
+
+    public void committed(int schedule, int idx){
+        for (int i = idx; i>=0; i--){
+            this.schedule.get(schedule).get(i).setStatus("COMMITTED");
+        }
+        System.out.println("Committed schedule "+ schedule);
+    }
 }
--- src/task/Task.java
+++ src/task/Task.java
@@ -1,25 +1,47 @@
 package task;
 
 public class Task {
+    private static int _queue = 1;
     private final int schedule;
     private final String task;
     private final String taskName;
+    private final int queue;
+    private String status;
 
     public Task(){
         this.schedule = 0;
         this.taskName = "";
         this.task = "";
+        this.status = "UNCOMMITTED";
+        this.queue = _queue;
+        _queue++;
     }
 
     public Task(String task, String taskName, int schedule){
         this.schedule = schedule;
         this.taskName = taskName;
         this.task = task;
+        this.status = "UNCOMMITTED";
+        this.queue = _queue;
+        _queue++;
+    }
+
+    public Task(String task, int schedule){
+        this.schedule = schedule;
+        this.task = task;
+        this.taskName = "";
+        this.status = "UNCOMMITTED";
+        this.queue = _queue;
+        _queue++;
     }
 
     public String getTask(){ return this.task;}
     public String getTaskName(){ return this.taskName;}
     public int getSchedule(){ return this.schedule;}
+    public String getStatus(){ return this.status;}
+    public int getQueue(){return this.queue;}
+
+    public void setStatus(String status){ this.status = status;}
 
 
     @Override

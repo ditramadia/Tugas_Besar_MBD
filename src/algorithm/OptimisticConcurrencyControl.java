cd0d001e3ac06234abbfb28e1b80fcda2b23d41c
christodharma <13521009@std.stei.itb.ac.id>
2023-12-01 11:43:46 +0700

    feat: OCC inits

---
+++ src/algorithm/OptimisticConcurrencyControl.java
@@ -0,0 +1,40 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import task.Task;
+import task.Transaction;
+
+/**
+ * OptimisticConcurrencyControl (OCC) class <p>
+ * 
+ */
+public class OptimisticConcurrencyControl {
+    private List<Task> tasks = new ArrayList<>();
+    private Transaction transaction;
+
+    /**
+     * Constructor <p>
+     * Melakukan parsing string dan memetakan task-task yang diparsing menjadi schedule
+     * @param transaction 1 string berisi task-task contohnya (R1(A) R2(B) W1(A) C1 ...) <p>
+     */
+    public OptimisticConcurrencyControl(String transaction){
+        String[] _temp = transaction.split(" ");
+        Pattern pattern1 = Pattern.compile("([A-Za-z])(\\d+)\\(([^)]+)\\)");
+        Pattern pattern2 = Pattern.compile("([A-Za-z])(\\d+)$");
+        for (String temp : _temp){
+            Matcher match1 = pattern1.matcher(temp);
+            while (match1.find()) {
+                this.tasks.add(new Task(match1.group(1),match1.group(3), Integer.parseInt(match1.group(2))));
+            }
+            Matcher match2 = pattern2.matcher(temp);
+            while (match2.find()) {
+                this.tasks.add(new Task(match2.group(1), Integer.parseInt(match2.group(2))));
+            }
+        }
+        this.transaction = new Transaction((Task[]) this.tasks.toArray());
+    }
+}
